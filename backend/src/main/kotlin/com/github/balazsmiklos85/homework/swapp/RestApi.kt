00459de44c250f88c49939220a2c6d6bfa365104
Miklós Balázs <balazsmiklos85+git@gmail.com>
2023-01-22 10:29:35 +0100

    Cross-Origin requests enabled in Spring Boot

--- backend/src/main/kotlin/com/github/balazsmiklos85/homework/swapp/RestApi.kt
+++ backend/src/main/kotlin/com/github/balazsmiklos85/homework/swapp/RestApi.kt
@@ -1,11 +1,13 @@
 package com.github.balazsmiklos85.homework.swapp
 
 import java.math.BigDecimal
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
+@CrossOrigin(origins = ["*"])
 @RestController
 class RestApi {
     @GetMapping("/data")
