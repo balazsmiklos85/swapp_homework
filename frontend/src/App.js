ad6d3972095498d1949aeb406c81f9eb0cb1026d
Miklós Balázs <balazsmiklos85+git@gmail.com>
2023-01-22 09:34:31 +0000

    Data loaded into a HTML table.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+// import 'antd/dist/antd.css'; Doesn't work. No idea why.
+import React from 'react';
+import { Button } from 'antd';
+import LoadTable from './LoadTable';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <LoadTable />
+      <Button>Click me</Button>
     </div>
   );
 }
---
+++ frontend/src/LoadTable.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
+function LoadTable() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8080/data')
+            .then(response => response.json())
+            .then(data => setData(data));
+    }, []);
+
+    return (
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Amount</th>
+                <th>Selected</th>
+            </tr>
+            {data.map(item => (
+            <tr>
+                <td>{item.name}</td>
+                <td>{item.amount}</td>
+                <td>
+                    <input type="checkbox" id="{item.name}-selected" value="false" />
+                </td>
+            </tr>
+            ))}
+          </table>
+    );
+}
+
+export default LoadTable;
