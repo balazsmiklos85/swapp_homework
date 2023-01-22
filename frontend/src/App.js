8031962ad85b4559300458eee9ccb5dbf4c11e69
Miklós Balázs <balazsmiklos85+git@gmail.com>
2023-01-22 10:04:03 +0000

    Invoice generation added to the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,36 @@
 import './App.css';
 // import 'antd/dist/antd.css'; Doesn't work. No idea why.
-import React from 'react';
-import { Button } from 'antd';
+import React, { useState } from 'react';
 import LoadTable from './LoadTable';
 
 function App() {
+    const [response, setResponse] = useState('');
+
+    const handleClick = async (event) => {
+        event.preventDefault()
+        setResponse(null);
+        const formData = new FormData(event.target);
+
+        const options = {
+            method: 'POST',
+            body: formData
+        };
+
+        const res = await fetch('http://localhost:8080/invoice', options);
+        const json = await res.json();
+        setResponse("http://localhost:8080/invoices/" + json);
+    }
+  
   return (
-    <div>
-      <LoadTable />
-      <Button>Click me</Button>
-    </div>
+    <form onSubmit={handleClick}>
+        <LoadTable />
+        <button type="submit">Generate invoice</button>
+        { response ? 
+        <div>
+            <a href={response}>Download generated invoice</a>
+        </div>
+        : null }
+    </form>
   );
 }
 
